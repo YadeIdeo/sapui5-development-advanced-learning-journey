482aef4d5d25effa647f36b00c048f6c2e1220c6
yade.acem@ideo-nl.com <yade.acem@ideo-nl.com>
2026-02-09 09:14:46 +0000

    fullscreenApp

--- webapp/controller/Carrier.controller.js
+++ webapp/controller/Carrier.controller.js
@@ -8,21 +8,18 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("student00.com.sap.training.ux402.fullscreen.ux402fullscreen.controller.Carrier", {
-            getRouter: function () {
 
-                return sap.ui.core.UIComponent.getRouterFor(this);
+            // get router function
+            getRouter: function (){
+                return sap.ui.core.UIComponent.getRouter(this);
             },
 
-            onPress: function(oEvent) {
-
+            onPress: function (oEvent){
                 var oItem = oEvent.getSource();
                 var oCtx = oItem.getBindingContext();
                 var sCarrid = oCtx.getProperty("Carrid");
-
-                this.getRouter().navTo("flights", {
-                    carrid: sCarrid
-                }, false);
-
+            
+                this.getRouter().navTo("flights", {carrid: sCarrid}, false)
             }
 
         });
