e763fd85b8a4cf66dd33c764965432c0ea01648b
yade.acem@ideo-nl.com <yade.acem@ideo-nl.com>
2026-02-09 11:07:09 +0000

    new commit

---
+++ webapp/control/HoverButton.js
@@ -0,0 +1,32 @@
+sap.ui.define([
+    "sap/m/Button"
+], function (Button) {
+    "use strict";
+
+    return Button.extend("student.com.sap.training.advancedsapui5.fullscreen.control.HoverButton", {
+        metadata: {
+            properties: {
+                "allowHover": {
+                    type: "boolean",
+                    defaultValue: false
+                },
+                "hoverText": {
+                    type: "string"
+                }
+            },
+            events: {
+                "hover": {}
+            }
+        },
+
+        onmouseover: function (evt) {
+            // if allowHover is true -> fire the hover event
+            if (this.getAllowHover) {
+                this.fireHover();
+            }
+        },
+
+        renderer: {}
+
+    });
+});
